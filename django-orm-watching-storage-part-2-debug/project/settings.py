9d25dbbecd13c18fcb70f4a47503c98b8ca3bf55
Pavelwell7 <pavelbugaev303@gmail.com>
2025-11-02 23:41:01 +0300

    Add files via upload

---
+++ django-orm-watching-storage-part-2-debug/project/settings.py
@@ -0,0 +1,34 @@
+import os
+from environs import Env
+
+env = Env()
+env.read_env()
+
+DATABASES = {
+    'default': {
+        'ENGINE': env.str('DB_ENGINE'),
+        'HOST': env.str('DB_HOST'),
+        'PORT': env.int('DB_PORT'),
+        'NAME': env.str('DB_NAME'),
+        'USER': env.str('DB_USER'),
+        'PASSWORD': env.str('DB_PASSWORD'),
+    }
+}
+INSTALLED_APPS = ['datacenter']
+SECRET_KEY: str = env('SECRET_KEY')
+DEBUG = env.bool('DEBUG', True)
+ROOT_URLCONF = 'project.urls'
+ALLOWED_HOSTS: list[str] = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+    },
+]
+USE_L10N = True
+LANGUAGE_CODE = 'ru-ru'
+TIME_ZONE = 'Europe/Moscow'
+USE_TZ = True
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
